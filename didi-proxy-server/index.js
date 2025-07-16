74071dac11fcf0f816ad6d079796927fb5a19315
shrashti-19 <shrashtidwivedi91@gmail.com>
2025-07-16 11:39:34 +0530

    context feature added

--- didi-proxy-server/index.js
+++ didi-proxy-server/index.js
@@ -12,12 +12,7 @@ app.post('/didi', async (req, res) => {
     const response = await axios.post(
       'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent',
       {
-        contents: [
-          {
-            role: "user",
-            parts: [{ text: req.body.prompt }]
-          }
-        ]
+        contents: req.body.contents
       },
       {
         headers: {
